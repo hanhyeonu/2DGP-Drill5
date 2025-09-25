db99ec51b4056984962d373eb1572168a1c8b69f
Hanu(LAPTOP) <hhw3078@gmail.com>
2025-09-25 21:15:44 +0900

    Drill #5 - while 문으로 기본 루프 작성.

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,29 +1,28 @@
 from pico2d import *
 
-
 open_canvas()
-grass = load_image('grass.png')
+TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
 def handle_events():
     global running
-
-    # fill here
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 
 running = True
-x = 800 // 2
-frame = 0
 
-# fill here
+while running:
+    clear_canvas()
+    TUK_ground.draw(400, 300)
+    update_canvas()
 
+    handle_events()
+    delay(0.05)
 
 close_canvas()
-
